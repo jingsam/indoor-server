550f58b69017e227c09955b712921bbdd6f57c21
jingsam <jing-sam@qq.com>
2016-12-19 00:48:37 +0800

    add ci

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # indoor-server
 Indoor maps server
+
+[![Build Status](https://travis-ci.org/jingsam/indoor-server.svg?branch=master)](https://travis-ci.org/jingsam/indoor-server)
--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const locations = require('./controllers/location')
 
 
-router.post('locations', locations.create)
-router.get('locations', locations.query)
+router.post('/locations', locations.create)
+router.get('/locations', locations.query)
 
 
 module.exports = router
